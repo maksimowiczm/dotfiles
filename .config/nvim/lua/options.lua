9e03a42265521c7b4f6d432aea8106a8fd7e9074
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-02-03 17:34:19 +0100

    add nvim

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "120"
+vim.opt.scrolloff = 3
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.textwidth = 120
+vim.o.undofile = true
+vim.o.termguicolors = true
---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		format_on_save = { timeout_ms = 500 },
+		formatters = {
+			shfmt = {
+				append_args = { "-i", "2" },
+			},
+		},
+	},
+}
+
